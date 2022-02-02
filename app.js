72e71ea9db37d2f6f592c97fcb35cc55ef0a930a
Doan Tran <doan7tran@gmail.com>
2022-02-02 12:02:55 +0100

    display categories, seller info

--- app.js
+++ app.js
@@ -16,30 +16,38 @@ app.get("/contact", (request, response, next) => {
 
 // using HBS
 app.get("/product-singer", (request, response, next) => {
-  const sewingDetails = {
+  const productDetails = {
     title: "Singer 44S",
     price: 280,
+    categories: ["beginner", "low price"],
     imgSrc:
       "https://i5.walmartimages.com/asr/55d35d78-f6c9-4382-a1e0-725c11ddfc16.d817e7733fee208b3101bccf3c5571d2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
   };
-  response.render("product", sewingDetails);
+  response.render("product", productDetails);
 });
 
 app.get("/product-w6", (request, response, next) => {
-  const sewingDetails = {
+  const productDetails = {
     title: "W6 3300",
     price: 350,
+    categories: ["intermediate", "low price"],
     imgSrc: "https://m.media-amazon.com/images/I/61HxZ5NoK7L._AC_SX466_.jpg",
+    seller: {
+      name: "Bob",
+      location: "Berlin",
+      rating: "4.5"
+    }
   };
-  response.render("product", sewingDetails);
+  response.render("product", productDetails);
 });
 
 app.get("/product-brother", (request, response, next) => {
-  const sewingDetails = {
+  const productDetails = {
     title: "Brother 2000",
     price: 550,
+    categories: ["advanced", "high price"]
   };
-  response.render("product", sewingDetails);
+  response.render("product", productDetails);
 });
 
 // app.get("/product", (request, response, next) => {
