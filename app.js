be65e81e4fdf9ccdb131feb209a0489f552f0667
dorrrn <doan7tran@gmail.com>
2022-02-03 14:48:12 +0100

    install mongoose, add Product model, get list of products from DB in home

--- app.js
+++ app.js
@@ -2,89 +2,69 @@ const express = require("express");
 const hbs = require("hbs");
 const app = express();
 
+const mongoose = require("mongoose");
+const Product = require("./models/Product.model");
+
 app.set("views", __dirname + "/views");
 app.set("view engine", "hbs");
 
 hbs.registerPartials(__dirname + "/views/partials");
 
-app.use(express.static("public")); // important: after const app = express() and before app.get()
+app.use(express.static("public"));
 
-app.get("/", (request, response, next) => {
-  const products = [
-    {
-      title: "Sewing machine 1",
-      price: 100,
-    },
-    {
-      title: "Sewing machine 2",
-      price: 180,
-    },
-    {
-      title: "Sewing machine 3",
-      price: 440,
-    },
-  ];
-  response.render("home", {products: products});
+// connect to DB
+mongoose
+  .connect("mongodb://0.0.0.0:27017/ecommerceApp")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+app.get("/", function (request, response, next) {
+  Product.find()
+    .then((products) => {
+      response.render("home", { products: products });
+    })
+    .catch( error => console.log('this is an error', error));
 });
 
-app.get("/contact", (request, response, next) => {
+app.get("/contact", function (request, response, next) {
   response.render("contact");
 });
 
-// using HBS
-app.get("/product-singer", (request, response, next) => {
-  const productDetails = {
-    title: "Singer 44S",
-    price: 280,
-    categories: ["beginner", "low price"],
-    imgSrc:
-      "https://i5.walmartimages.com/asr/55d35d78-f6c9-4382-a1e0-725c11ddfc16.d817e7733fee208b3101bccf3c5571d2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
-  };
-  response.render("product", productDetails);
-});
-
-app.get("/product-w6", (request, response, next) => {
-  const productDetails = {
-    title: "W6 3300",
-    price: 350,
-    categories: ["intermediate", "low price"],
-    imgSrc: "https://m.media-amazon.com/images/I/61HxZ5NoK7L._AC_SX466_.jpg",
+app.get("/product-boat", function (request, response, next) {
+  const boatDetails = {
+    title: "Boat",
+    price: 100,
     seller: {
-      name: "Bob",
-      location: "Berlin",
-      rating: "4.5",
+      name: "bob",
+      rating: 4.5,
     },
   };
-  response.render("product", productDetails);
+  response.render("product", boatDetails);
 });
 
-app.get("/product-brother", (request, response, next) => {
-  const productDetails = {
-    title: "Brother 2000",
-    price: 550,
-    categories: ["advanced", "high price"],
+app.get("/product-jet-ski", function (request, response, next) {
+  const jetSkiDetails = {
+    title: "Jet Ski",
+    price: 3000,
+    imgSrc:
+      "https://checkyeti.imgix.net/images/prod/products/16829/jet-ski-hire-on-kamari-beach-in-santorini-kamari-beach-watersports-santorini.jpg",
+    categories: ["leisure", "sports"],
   };
-  response.render("product", productDetails);
+  response.render("product", jetSkiDetails);
 });
 
-// app.get("/product", (request, response, next) => {
-//   const sewingDetails = [
-//     {
-//       title: "Singer 44S",
-//       price: 280,
-//       img: "https://i5.walmartimages.com/asr/55d35d78-f6c9-4382-a1e0-725c11ddfc16.d817e7733fee208b3101bccf3c5571d2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
-//     },
-//     {
-//       title: "W6 3300",
-//       price: 350,
-//       img: "https://m.media-amazon.com/images/I/61HxZ5NoK7L._AC_SX466_.jpg"
-//     },
-//   ];
-
-//   response.render("product", sewingDetails);
-// });
-
-app.listen(3003),
-  () => {
-    console.log("server listening ...");
+app.get("/product-yatch", function (request, response, next) {
+  const yatchDetails = {
+    title: "Yatch",
+    price: 856000,
+    imgSrc: "https://yachtharbour.com/static/images/y/large_146_0d06c.jpg",
+    categories: ["luxury", "bart", "toys"],
   };
+  response.render("product", yatchDetails);
+});
+
+app.listen(3000, () => {
+  console.log("server listening....");
+});
---
+++ db-example.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const Product = require("./models/Product.model");
+
+//
+// DB CONNECTION
+// !! localhost: 0.0.0.0:27017 for me
+mongoose
+  .connect("mongodb://0.0.0.0:27017/ecommerceApp")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
+
+//
+// CREATE
+//
+
+const boatDetails1 = {
+  title: "basic boat",
+  price: 100,
+  color: "pink",
+};
+
+const boatDetails2 = {
+  title: "jet ski",
+  price: 500,
+  color: "pink",
+};
+
+const boatDetails3 = {
+  title: "yacht",
+  price: 102000,
+  color: "pink",
+};
+
+Product.insertMany([boatDetails1, boatDetails2, boatDetails3])
+  .then((result) => console.log(result))
+  .catch();
+
+//
+// READ
+//
+// Product.find()
+//   .then((result) => console.log(result.length))
+//   .catch((error) => console.log(error));
+
+// //
+// // UPDATE
+// //
+// Product.findByIdAndUpdate("61fbaea5ac564cf6aa421bfd", { price: 500 })
+//   .then()
+//   .catch();
+
+//
+// DELETE
+//
+
+// Product.deleteOne({title: "basic boat"})
+// .then( result => console.log("document deleted"))
+// .catch();
